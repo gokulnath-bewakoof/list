82edb597b90f738f06310d4103d63eea27ad7684
Gokulnath <gokulnath.s@bewakoof.com>
2022-08-02 13:57:12 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const userData = [
+    { name: "Jeans" },
+    { name: "Sirt" },
+    { name: "Black Formals" },
+    { name: "Veggies" },
+    { name: "Fruit" }
+  ];
+  
+  function App() {
+    const [users, setUsers] = useState([]);
+  
+    useEffect(() => {
+      setUsers(userData);
+    }, []);
+  
+    const handleChange = (e) => {
+      const { name, checked } = e.target;
+      if (name === "allSelect") {
+        let tempUser = users.map((user) => {
+          return { ...user, isChecked: checked };
+        });
+        setUsers(tempUser);
+      } else {
+        let tempUser = users.map((user) =>
+          user.name === name ? { ...user, isChecked: checked } : user
+        );
+        setUsers(tempUser);
+      }
+    };
+  
+    return (
+      <div className="container my-4" style={{ width: "500px" }}>
+        <form className="form w-100">
+          <h3>To Do List</h3>
+          <div className="form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              name="allSelect"
+              checked={!users.some((user) => user?.isChecked !== true)}
+              onChange={handleChange}
+            />
+            <label className="form-check-label ms-2">All Select</label>
+          </div>
+          {users.map((user, index) => (
+            <div className="form-check" key={index}>
+              <input
+                type="checkbox"
+                className="form-check-input"
+                name={user.name}
+                checked={user?.isChecked || false}
+                onChange={handleChange}
+              />
+              <label className="form-check-label ms-2">{user.name}</label>
+            </div>
+          ))}
+        </form>
+        
+      </div>
+    );
+  }
+  
+  export default App;
+  
